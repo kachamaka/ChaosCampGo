9e0ff6109f28d2f503d207fafbb0c5cb8a2bc2a2
unknown <martin.popov42@gmail.com>
2023-01-28 22:39:32 +0200

    initial commit

---
+++ server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/login", headers)
+	http.HandleFunc("/register", headers)
+	http.HandleFunc("/getEvents", hello)
+	http.HandleFunc("/addEvent", hello)
+	http.HandleFunc("/deleteEvent", hello)
+
+	http.ListenAndServe(":8888", nil)
+}
